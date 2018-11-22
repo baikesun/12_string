93a535cfec769874d25ee42bffafa6d22dddc3b8
baikesun <curious98@naver.com>
2018-11-22 15:15:36 +0900

    01

---
+++ main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	char src[100] = "Programming course";
+	char dst[100];
+	int i=0;
+	/*
+	for(i=0; src[i] != '\0'; i++)
+	{
+		dst[i] = src[i];
+	}
+	*/
+	
+	while(src[i] != '\0')
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	
+	dst[i] = '\0';
+	
+	printf("dst : %s\n", dst);
+	
+	return 0;
+}
